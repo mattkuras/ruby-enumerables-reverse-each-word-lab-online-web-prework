360e8b6b975b0d2f5b8146ac6f7abd1f53aa692d
Matt Kuras <mattkuras@gmail.com>
2020-02-21 20:05:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,13 @@ def reverse_each_word(sentence)
 arr = sentence.split 
 arr.each do |item| new_arr << item.reverse end 
 new_arr.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  new_arr = []
+arr = sentence.split 
+arr.each do |item| new_arr << item.reverse end 
+new_arr.join(" ")
 
 end
