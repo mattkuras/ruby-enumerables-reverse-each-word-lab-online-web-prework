857fe8dc55f56eb71500284dcd70f24bb7f29d7d
Matt Kuras <mattkuras@gmail.com>
2020-02-21 19:59:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_arr = []
 arr = sentence.split 
-arr.each do new_arr |item| << item.reverse end 
+arr.each do |item| new_arr << item.reverse end 
 new_arr
 
 end
